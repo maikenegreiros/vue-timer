8f9b2e9841644755f4cf7b3071759db78d953e0a
Maike <maike_jbe@hotmail.com>
2018-06-16 14:54:43 -0300

    Changes to work with single file components

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,63 +1,9 @@
+import Vue from 'vue'
+import Timer from '../components/Timer'
+
 window.addEventListener("DOMContentLoaded", () => {
-    const start = function() {
-        let hours = document.querySelector('.hours').value
-        let minutes = document.querySelector('.minutes').value
-        let seconds = document.querySelector('.seconds').value
-
-        let timerId = setInterval(() => {
-            countDown()
-        }, 1000)
-
-        const countDown = () => {
-            if (this.hours == 0 && this.minutes == 0 && this.seconds == 0) {
-                finish()
-            } else {
-                countDownSeconds()
-            }
-        }
-
-        const countDownHours = () => {
-            this.hours --
-        }
-
-        const countDownMinutes = () => {
-            if (this.minutes == 0) {
-                countDownHours()
-                this.minutes = 60
-            }
-            this.minutes --
-        }
-
-        const countDownSeconds = () => {
-            if (this.seconds == 0) {
-                countDownMinutes()
-                this.seconds = 60
-            }
-            this.seconds --
-        }
-
-        const finish = () => {
-            console.log("O tempo acabou")
-            clearInterval(timerId)
-        }
-    }
-
-
-    var app = new Vue({
+    const app = new Vue({
         el: '#app',
-        data: {
-            title: 'Timer',
-            hours:"00",
-            minutes:"00",
-            seconds:"00",
-        },
-        methods: {
-            countDown: start,
-            reset: function() {
-                this.hours = "00"
-                this.minutes = "00"
-                this.seconds = "00"
-            }
-        }
+        components: { Timer }
     })
 })
