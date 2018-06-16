249f83806e5e1e79c55444d9382f3ce9fe55be32
Maike <maike_jbe@hotmail.com>
2018-06-16 14:53:25 -0300

    Added a single file component

---
+++ assets/components/Timer.vue
@@ -0,0 +1,15 @@
+<template>
+    <div id="app">
+        <div class="timer-display">
+            <p>
+                <input v-model="hours" class="hours" type="number" value="00">:
+                <input v-model="minutes" class="minutes" type="number" value="00">:
+                <input v-model="seconds" class="seconds" type="number" value="00">
+            </p>
+        </div>
+        <button v-on:click="countDown" type="button" name="button">Começar</button>
+        <button v-on:click="reset" type="button" name="button">Reset</button>
+    </div>
+</template>
+
+<script src="../js/Timer.js"></script>
